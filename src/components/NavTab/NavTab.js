a764210d491f4b4a7bc6a13e2661f31586738e99
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-21 17:21:29 +0300

    refactor navtab add react scroll

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,44 +1,35 @@
 import React from 'react';
 import './NavTab.css'
+import { Link } from 'react-scroll';
 
 
 function NavTab() {
-  const navItems = [
-    { id: 1, text: 'О проекте' },
-    { id: 2, text: 'Технологии' },
-    { id: 3, text: 'Студент' },
-  ];
-
   return (
     <section className='navtab'>
       <nav className='navtab__container'>
         <ul className='navtab__navigation'>
-          {navItems.map((item) => (
-            <li className='navtab__text' key={item.id}>
-              {item.text}
-            </li>
-          ))}
+          <li className='navtab__text'>
+            <Link to="project" smooth={true} >
+              О проекте
+            </Link>
+          </li>
+          <li className='navtab__text'>
+            <Link to="techs" smooth={true} >
+              Технологии
+            </Link>
+          </li>
+          <li className='navtab__text'>
+            <Link to="student" smooth={true}>
+              Студент
+            </Link>
+          </li>
         </ul>
       </nav>
     </section>
   );
 }
-// function NavTab(){
-//     const AboutProjectTitle = 'О проекте'
-//     const TechsTitle = 'Технологии'
-//     const AboutMeText = 'Студент'
 
-//     return(
-// <section className='navtab'>
-//       <nav className='navtab__container'>
-//         <ul className='navtab__navigation'>
-//             <li className='navtab__text'>{AboutProjectTitle}</li>
-//             <li className='navtab__text'>{TechsTitle}</li>
-//             <li className='navtab__text'>{AboutMeText}</li>
-//         </ul>
-//       </nav>
-//     </section>
-//     )
-// }
 
 export default NavTab;
+
+
