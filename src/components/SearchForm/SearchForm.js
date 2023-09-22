3c577aacdeed8bd3171db24a36466175abe06ce1
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-22 14:11:14 +0300

    add focus for navigation

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,7 +2,23 @@ import React from 'react';
 import './SearchForm.css';
 
 function SearchForm() {
-  return <section className="search"></section>;
+  return (
+    <section className="footer">
+      <h2 className="footer__title">{}</h2>
+      <p className="footer__line"></p>
+      <div className="footer__content">
+        <p className="footer__year">&copy;{}</p>
+        <ul className="footer__text">
+          <li className="footer__subtitle">{}</li>
+          <li>
+            <a href="" className="footer__link" target="_blank" rel="noopener noreferrer">
+              {}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default SearchForm;
