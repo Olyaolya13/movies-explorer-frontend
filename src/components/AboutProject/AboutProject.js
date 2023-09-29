7ac6c6fc232bf5fab3ce88fb953364aeb56b41f6
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-09-29 12:19:12 +0300

    add media about project

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,57 +1,32 @@
 import React from 'react';
-import './AboutProject.css'
+import './AboutProject.css';
+import { AboutProjectData } from '../../utils/constants';
 
-const aboutProjectData = {
-    title: 'О проекте',
-    stages: [
-      {
-        title: 'Дипломный проект включал 5 этапов',
-        description: 'Составление плана, работа над бэкендом, вёрстку, добавление функциональности и финальные доработки.',
-      },
-      {
-        title: 'На выполнение диплома ушло 5 недель',
-        description: 'У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.',
-      },
-    ],
-    timeframes: {
-      oneWeek: '1 неделя',
-      fourWeeks: '4 недели',
-    },
-    skills: ['Back-end', 'Front-end'],
-  };
-  
-  const OneWeek = aboutProjectData.timeframes.oneWeek;
-  const Back = aboutProjectData.skills[0];
-  const Foureek = aboutProjectData.timeframes.fourWeeks;
-  const Front = aboutProjectData.skills[1];
-  
-  function AboutProject() {
-    return (
-      <section className='project'>
-          <h2 className='project__title'>{aboutProjectData.title}</h2>
-          <p className='project__line'></p>
-        <div className='project__content'>
-          {aboutProjectData.stages.map((stage, index) => (
-            <div className='project__text' key={index}>
-              <h3 className='project__subtitle'>{stage.title}</h3>
-              <p className='project__about'>{stage.description}</p>
-            </div>
-          ))}
+function AboutProject() {
+  return (
+    <section className="project">
+      <h2 className="project__title">{AboutProjectData.title}</h2>
+      <p className="project__line"></p>
+      <div className="project__content">
+        {AboutProjectData.stages.map((stage, index) => (
+          <div className="project__text" key={index}>
+            <h3 className="project__subtitle">{stage.title}</h3>
+            <p className="project__about">{stage.description}</p>
+          </div>
+        ))}
+      </div>
+      <div className="project__develop">
+        <div className="project__tb">
+          <p className="project__week">{AboutProjectData.timeframes[0]}</p>
+          <p className="project__name">{AboutProjectData.skills[0]}</p>
         </div>
-        <div className='project__develop'>
-        <div className='project__tb'>
-          <p className='project__week'>{OneWeek}</p>
-          <p className='project__name'>{Back}</p>
+        <div className="project__tb1">
+          <p className="project__weeks">{AboutProjectData.timeframes[1]}</p>
+          <p className="project__name">{AboutProjectData.skills[1]}</p>
         </div>
-        <div className='project__tb'>
-          <p className='project__weeks'>{Foureek}</p>
-          <p className='project__name'>{Front}</p>
-        </div>
-        </div>
-      </section>
-    );
-  }
-       
-
+      </div>
+    </section>
+  );
+}
 
 export default AboutProject;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,6 +33,25 @@ export const NavTabData = {
   student: 'Студент'
 };
 
+//About projects
+export const AboutProjectData = {
+  title: 'О проекте',
+  stages: [
+    {
+      title: 'Дипломный проект включал 5 этапов',
+      description:
+        'Составление плана, работа над бэкендом, вёрстку, добавление функциональности и финальные доработки.'
+    },
+    {
+      title: 'На выполнение диплома ушло 5 недель',
+      description:
+        'У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.'
+    }
+  ],
+  timeframes: ['1 неделя', '4 недели'],
+  skills: ['Back-end', 'Front-end']
+};
+
 //Movies
 export const MoviesCardListData = [
   {
